a09c6b27c189c0af12f1555df8136ee6d16f6167
asha <asha88ptz@gmail.com>
2023-01-18 23:57:31 +0300

    Conditions task1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean t = true;
+        if (t) {
+            System.out.println("true");
+        }
     }
 }
